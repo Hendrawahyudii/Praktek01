8b4315daa0f4df2efd62fe0a5be10bb967e79e42
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-03-26 17:38:52 +0700

    Membuat Atribut dan Behavior

--- src/praktek01/Sendal.java
+++ src/praktek01/Sendal.java
@@ -10,5 +10,18 @@ package praktek01;
  * @author Hndrw
  */
 public class Sendal {
+    String warna;
+    String merk;
+    String bahan;
+    String jenis;
+    String ukuran;
     
-}
+    void cetakInfo(){
+        System.out.println("Warna :"+warna);
+        System.out.println("Merk :"+merk);
+        System.out.println("Bahan :"+bahan);
+        System.out.println("Jenis :"+jenis);
+        System.out.println("Ukuran :"+ukuran);
+    }
+    }
+
