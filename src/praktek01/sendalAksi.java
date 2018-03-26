ac636b69df09f32e680a79beb0724b849edd29be
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-03-26 17:56:15 +0700

    Mengisi nilai atribut

--- src/praktek01/sendalAksi.java
+++ src/praktek01/sendalAksi.java
@@ -11,8 +11,26 @@ package praktek01;
  */
 public class sendalAksi {
     public static void main(String[] args) {
-        Sendal Nippon= new Sendal();
-        Sendal Croccodile= new Sendal();
-        Sendal Eiger = new Sendal ();
+        Sendal Capal= new Sendal();
+        Sendal Casual= new Sendal();
+        Sendal Outdoor= new Sendal ();
+        
+        Capal.warna="Biru";
+        Capal.merk="Nippon";
+        Capal.bahan="Karet";
+        Capal.jenis="Jepit";
+        Capal.ukuran="38 39 40";
+        
+        Casual.warna="Coklat";
+        Casual.merk="Crocodile";
+        Casual.bahan="Kulit";
+        Casual.jenis="PDH";
+        Casual.ukuran="39 40 41";
+        
+        Outdoor.warna="Hitam";
+        Outdoor.merk="Eiger";
+        Outdoor.bahan="Kain Karet";
+        Outdoor.jenis="Sendal Gunung";
+        Outdoor.ukuran="39 40 41 42";
     }
 }
